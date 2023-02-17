0cff8893a5e074a822a4a88884eb7825510914d6
GRS.Jpeg <pochtagrs@mail.ru>
2023-02-17 21:13:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
+import Home from './pages/Home'; 
 import NotFound from './pages/NotFound';
 import { Routes, Route } from 'react-router-dom';
 import CartPage from './pages/CartPage';
